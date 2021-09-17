c955a8a5a3aa0af010f1395b409296e91e372ddc
Scott Minor <minor@minorhacks.com>
2021-09-17 12:48:38 -0700

    Error logging for failing handlers

--- fuse/fs.go
+++ fuse/fs.go
@@ -1,6 +1,7 @@
 package fuse
 
 import (
+	"syscall"
 	"time"
 
 	"github.com/golang/glog"
@@ -19,9 +20,25 @@ func (f *GitFS) SetDebug(debug bool) {
 	// TODO: implement
 }
 
-func (f *GitFS) GetAttr(name string, context *gofuse.Context) (*gofuse.Attr, gofuse.Status) {
-	// TODO: implement
+func (f *GitFS) GetAttr(name string, context *gofuse.Context) (ret *gofuse.Attr, status gofuse.Status) {
 	glog.V(1).Infof("GetAttr(name=%q) called", name)
+	defer func() {
+		if status != gofuse.OK {
+			glog.Errorf("GetAttr(name=%q) failed: %v", name, status)
+		}
+	}()
+
+	// Simulate top-level dirs
+	switch name {
+	case "":
+		return &gofuse.Attr{
+			Mode: syscall.S_IFDIR,
+		}, gofuse.OK
+	case "commits":
+		return &gofuse.Attr{
+			Mode: syscall.S_IFDIR,
+		}, gofuse.OK
+	}
 	return nil, gofuse.ENOENT
 }
 
@@ -41,9 +58,14 @@ func (f *GitFS) Truncate(name string, size uint64, context *gofuse.Context) gofu
 	return gofuse.EROFS
 }
 
-func (f *GitFS) Access(name string, mode uint32, context *gofuse.Context) gofuse.Status {
+func (f *GitFS) Access(name string, mode uint32, context *gofuse.Context) (status gofuse.Status) {
 	// TODO: Implement
 	glog.V(1).Infof("Access(name=%q, mode=%#o) called", name, mode)
+	defer func() {
+		if status != gofuse.OK {
+			glog.Errorf("Access(name=%q) failed: %v", name, status)
+		}
+	}()
 	return gofuse.ENOENT
 }
 
@@ -71,16 +93,29 @@ func (f *GitFS) Unlink(name string, context *gofuse.Context) gofuse.Status {
 	return gofuse.EROFS
 }
 
-func (f *GitFS) GetXAttr(name string, attribute string, context *gofuse.Context) ([]byte, gofuse.Status) {
-	// TODO: implement
+func (f *GitFS) GetXAttr(name string, attribute string, context *gofuse.Context) (xattr []byte, status gofuse.Status) {
 	glog.V(1).Infof("GetXAttr(name=%q, attribute=%q) called", name, attribute)
-	return nil, gofuse.ENOENT
+	defer func() {
+		if status != gofuse.OK {
+			glog.Errorf("GetXAttr(name=%q) failed: %v", name, status)
+		}
+	}()
+
+	// Assume there are never any extended attributes for now.
+	return nil, gofuse.OK
 }
 
-func (f *GitFS) ListXAttr(name string, context *gofuse.Context) ([]string, gofuse.Status) {
+func (f *GitFS) ListXAttr(name string, context *gofuse.Context) (xattrs []string, status gofuse.Status) {
 	// TODO: implement
 	glog.V(1).Infof("ListXAttr(name=%q) called", name)
-	return nil, gofuse.ENOENT
+	defer func() {
+		if status != gofuse.OK {
+			glog.Errorf("ListXAttr(name=%q) failed: %v", name, status)
+		}
+	}()
+
+	// Assume there are never any extended attributes for now.
+	return nil, gofuse.OK
 }
 
 func (f *GitFS) RemoveXAttr(name string, attr string, context *gofuse.Context) gofuse.Status {
@@ -99,9 +134,14 @@ func (f *GitFS) OnUnmount() {
 	glog.V(1).Infof("OnUnmount() called")
 }
 
-func (f *GitFS) Open(name string, flags uint32, context *gofuse.Context) (nodefs.File, gofuse.Status) {
+func (f *GitFS) Open(name string, flags uint32, context *gofuse.Context) (file nodefs.File, status gofuse.Status) {
 	// TODO: implement
 	glog.V(1).Infof("Open(name=%q, flags=%#x) called", name, flags)
+	defer func() {
+		if status != gofuse.OK {
+			glog.Errorf("Open(name=%q) failed: %v", name, status)
+		}
+	}()
 	return nil, gofuse.ENOENT
 }
 
@@ -109,9 +149,25 @@ func (f *GitFS) Create(name string, flags uint32, mode uint32, context *gofuse.C
 	return nil, gofuse.EROFS
 }
 
-func (f *GitFS) OpenDir(name string, context *gofuse.Context) ([]gofuse.DirEntry, gofuse.Status) {
-	// TODO: implement
+func (f *GitFS) OpenDir(name string, context *gofuse.Context) (dirs []gofuse.DirEntry, status gofuse.Status) {
 	glog.V(1).Infof("OpenDir(name=%q) called", name)
+	defer func() {
+		if status != gofuse.OK {
+			glog.Errorf("OpenDir(name=%q) failed: %v", name, status)
+		}
+	}()
+
+	// Simulate top-level dirs
+	switch name {
+	case "":
+		return []gofuse.DirEntry{
+			{
+				Name: "commits",
+				Mode: syscall.S_IFDIR,
+			},
+		}, gofuse.OK
+	}
+
 	return nil, gofuse.ENOENT
 }
 
@@ -119,9 +175,14 @@ func (f *GitFS) Symlink(value string, linkName string, context *gofuse.Context) 
 	return gofuse.EROFS
 }
 
-func (f *GitFS) Readlink(name string, context *gofuse.Context) (string, gofuse.Status) {
+func (f *GitFS) Readlink(name string, context *gofuse.Context) (link string, status gofuse.Status) {
 	// TODO: implement
 	glog.V(1).Infof("Readlink(name=%q) called", name)
+	defer func() {
+		if status != gofuse.OK {
+			glog.Errorf("Readlink(name=%q) failed: %v", name, status)
+		}
+	}()
 	return "", gofuse.ENOENT
 }
 
